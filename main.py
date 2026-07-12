e7d33dedacc8ea8bf883a0bbdd11bad7d64024dd
deathstillerlol-jpg <death.stiller.lol@gmail.com>
2026-07-12 14:48:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ init_db()
 async def cmd_start(message: Message):
     save_user(message.from_user.id)
     kb = InlineKeyboardMarkup(inline_keyboard=[[
-        InlineKeyboardButton(text="НАПИСАТЬ МЕНЕДЖЕРУ", url="https://t.me/sasha_ticket")
+        InlineKeyboardButton(text="НАПИСАТЬ МЕНЕДЖЕРУ", url="https://t.me/sasha_teatr")
     ]])
     await message.answer(
         "Здравствуйте! Приветствуем вас! 👋\n"
@@ -87,7 +87,7 @@ async def broadcaster():
 
     text = "Напоминание! 🔥\n\nБЫСТРЕЕ ПИШЕМ!\nПиши менеджеру прямо сейчас 👇"
     kb = InlineKeyboardMarkup(inline_keyboard=[[
-        InlineKeyboardButton(text="НАПИСАТЬ МЕНЕДЖЕРУ →", url="https://t.me/sasha_ticket")
+        InlineKeyboardButton(text="НАПИСАТЬ МЕНЕДЖЕРУ →", url="https://t.me/sasha_teatr")
     ]])
 
     while True:
